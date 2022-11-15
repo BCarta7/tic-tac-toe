c8f4925ae8ed3395e87a7032435b7b94876e1ec7
BCarta7 <cannolidude@msn.com>
2022-11-15 10:13:34 -0800

    all user stories complete

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const App = () => {
       const [a, b, c] = lines[i];
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
         return squares[a];
-      }
+      } 
     }
     return null;
   }
@@ -48,13 +48,15 @@ const App = () => {
     status = "VICTORY FOR PLAYER 1"
   } else if (winner === "O") {
     status = "VICTORY FOR PLAYER 2"
+  } else if (counter === 11) {
+    status = "STALEMATE"
   }
-console.log(status)
 
   return (
     <>
       <h1>Tic Tac Toe</h1>
       <p>Player 1 gets X. Player 2 gets O</p>
+      <button className="restart" onClick={() => window.location.reload(false)}>Start new game</button>
       <h2>{status}</h2>
       <div className="gameboard">
       {squares.map((value, index) => {
--- src/components/Square.js
+++ src/components/Square.js
@@ -5,13 +5,10 @@ const Square = (props) => {
   const handleClick = () => {
     props.handleGamePlay(props.index)
     props.plus()
-    if (props.winner === true) {
-      return alert("we have a winner!")
-    }
   }
 
   return (
-    <button className="square" onClick={handleClick} disabled={props.value === 'X' || props.value === 'O'}>{props.value}</button>
+    <button className="square" onClick={handleClick} disabled={props.value === 'X' || props.value === 'O' || props.winner === "X" || props.winner === "O"}>{props.value}</button>
   )
 }
 export default Square
